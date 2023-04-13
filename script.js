1cf479172392bd239bbddf2f3a41da234cfa7019
Rajkirantagde <121598976+rajutagde@users.noreply.github.com>
2023-04-13 19:57:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+let bandNames = ['The Beatles', 'Led Zeppelin', 'Pink Floyd', 'The Who', 'Aerosmith', 'Rolling Stones', 'Queen', 'Anthrax', 'Black Sabbath'];
+
+function sortBands(bands) {
+  const articles = ['the', 'a', 'an'];
+  const sortedBands = bands.sort(function(a, b) {
+    const nameA = a.toLowerCase().replace(/^(the|an|a)\s+/, '');
+    const nameB = b.toLowerCase().replace(/^(the|an|a)\s+/, '');
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+    return 0;
+  });
+  return sortedBands;
+}
+
+bandNames = sortBands(bandNames);
+
+const bandList = document.getElementById('band');
+bandNames.forEach(function(band) {
+  const li = document.createElement('li');
+  li.appendChild(document.createTextNode(band));
+  bandList.appendChild(li);
+});
